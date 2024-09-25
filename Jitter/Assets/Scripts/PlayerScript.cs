5b2af54d287d3befe762b02ecc7c23853676f47f
JJCarkcelo <80934424+JJCarkcelo@users.noreply.github.com>
2024-09-25 02:25:24 -0700

    further attempts to scripting for multiple action maps
    so much pain

--- Jitter/Assets/Scripts/PlayerScript.cs
+++ Jitter/Assets/Scripts/PlayerScript.cs
@@ -5,59 +5,58 @@ using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    //move speed
-    public float moveSpeed = 10.0f;
-    private Vector3 movementInput;
-    //jumping related
-    public float jumpForce = 2f;
-    public float jumpCount = 1;
-    public float maxJumpCount = 1;
-    //physics
-    private Rigidbody rb;
+    //testing bc pain
     private bool isGrounded;
-    //input sys -> action
-    public InputAction playerInputs;
-    private playerInputs unequipped;
-    private playerInputs knife;
-    private playerInputs rifle;
+       
+    PlayerInputs playerInputs;
+    public unequippedMap currentMap;
+    
+    InputAction move;
 
-    public struct playerActionStruct
+    private void Awake()
     {
-        public PlayerInput inputActions;
+        playerInputs = new InputSystem();
+
     }
+
 
     private void OnEnable()
-    { 
-        public playerInput.unequipped.move; //WASD
-        public playerInput.unequipped.jump; //space
-        public playerInput.unequipped.run; //shift
-        public playerInput.unequipped.crouchSlide; //crtl or c
-        public playerInput.unequipped.focus; //Q or RMB
-        public playerInput.unequipped.punch; //V or LMB
-
-        public playerInput.knife.move;
-        public playerInput.knife.jump; 
-        public playerInput.knife.run;
-        public playerInput.knife.crouchSlide;
-        public playerInput.knife.focus;
-        public playerInput.knife.lightAttack;
-        public playerInput.knife.heavyAttack;
-
-        public playerInput.rifle.move;
-        public playerInput.rifle.jump; 
-        public playerInput.rifle.run;
-        public playerInput.rifle.crouchSlide;
-        public playerInput.rifle.punch;
-        public playerInput.rifle.ads;
-        public playerInput.rifle.shoot
-        public playerInput.rifle.reload;
-
-
-    }
-
-    void Awake()
     {
-        inputActions = new InputSystem();
-        inputActions.Enable();
+        currentMap.Unequipped.Move;
+        playerInputs.
     }
-}
+
+    public struct unequippedMap
+    {
+        public InputAction move;
+        public InputAction jump;
+        public InputAction run;
+        public InputAction crouchSlide;
+        public InputAction focus;
+        public InputAction punch;
+        
+    }
+    public struct knifeMap
+    {
+        public InputAction move;
+        public InputAction jump;
+        public InputAction run;
+        public InputAction crouchSlide;
+        public InputAction focus;
+        public InputAction lightAttack;
+        public InputAction heavyAttack;
+    }
+
+    public struct rifleMap
+    {
+        public InputAction move;
+        public InputAction jump;
+        public InputAction run;
+        public InputAction crouchSlide;
+        public InputAction punch;
+        public InputAction ads;
+        public InputAction shoot;
+        public InputAction reload;
+    }
+
+    
